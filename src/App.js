59fc68df51315f9dedc0e86e616181d23896aae0
Matthew Ghannoum <mattghanno@gmail.com>
2021-04-20 17:21:53 +1000

    Created sidebar and main panel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MainPanel from "components/MainPanel";
+import SideBar  from "components/SideBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change 2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SideBar />
+
+      <MainPanel />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/SideBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="green side-bar">
+      <div style={{width: "100%"}}>
+        <h1>Imported Melanoma Imaging Sets</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
