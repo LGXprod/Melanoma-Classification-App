0d4e117729a7b917443f279004f6c08eb87d87b3
Matthew Ghannoum <mattghanno@gmail.com>
2021-06-09 09:57:49 +1000

    Single patient red form message and forms now show imported file(s)

--- src/layouts/MainPanel/forms/BatchImport.js
+++ src/layouts/MainPanel/forms/BatchImport.js
@@ -16,12 +16,23 @@ const BatchImport = ({ setSelectedTab }) => {
   const [images, setImages] = useState();
 
   useEffect(() => {
-    console.log(csv);
-  }, [csv]);
+    console.log("xx", images);
+    if (!Array.isArray(images) && images != null) {
+      const imageList = [];
+
+      for (let image of images) {
+        imageList.push(image);
+      }
+
+      setImages(imageList);
+    }
+  }, [images]);
 
   const updateDetails = (attribute, value) => {
     setFormDetails({ ...formDetails, [attribute]: value });
   };
+
+  const [showUpload, setShowUpload] = useState(false);
 
   const submitForm = () => {
     let imageData = {};
@@ -59,46 +70,71 @@ const BatchImport = ({ setSelectedTab }) => {
 
         <h1 style={{ marginTop: "-30px" }}>Batch Patient Detection</h1>
 
-        <div>
-          <TextField
-            id="standard-basic"
-            label="Batch name"
-            onChange={(e) => updateDetails("batchName", e.target.value)}
-          />
-        </div>
+        {showUpload ? (
+          <>
+            <label>CSV file imported: {csv.name}</label>
 
-        <div>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div style={{ marginRight: "5px" }}>
-              <FileDialog
-                isSingleFile={true}
-                label="&nbsp;&nbsp;&nbsp;Import CSV&nbsp;&nbsp;&nbsp;"
-                setFiles={setCsv}
+            <label>Images imported:</label>
+
+            <div style={{ height: "125px", overflowY: "scroll" }}>
+              {images.map((image, i) => {
+                return <label key={i} style={{display: "block"}}>{image.name}</label>;
+              })}
+            </div>
+
+            <div>
+              <Button className="reg-button" onClick={submitForm}>
+                &nbsp;&nbsp;Confirm&nbsp;&nbsp;
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div>
+              <TextField
+                id="standard-basic"
+                label="Batch name"
+                onChange={(e) => updateDetails("batchName", e.target.value)}
               />
             </div>
-            <div style={{ marginLeft: "5px" }}>
-              <FileDialog
-                isSingleFile={false}
-                label="Import Images"
-                setFiles={setImages}
-              />
-            </div>
-          </div>
-        </div>
 
-        <div>
-          <Button className="reg-button" onClick={submitForm}>
-            &nbsp;&nbsp;Submit&nbsp;&nbsp;
-          </Button>
-        </div>
+            <div>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ marginRight: "5px" }}>
+                  <FileDialog
+                    isSingleFile={true}
+                    label="&nbsp;&nbsp;&nbsp;Import CSV&nbsp;&nbsp;&nbsp;"
+                    setFiles={setCsv}
+                  />
+                </div>
+                <div style={{ marginLeft: "5px" }}>
+                  <FileDialog
+                    isSingleFile={false}
+                    label="Import Images"
+                    setFiles={setImages}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <Button
+                className="reg-button"
+                onClick={() => setShowUpload(true)}
+              >
+                &nbsp;&nbsp;Submit&nbsp;&nbsp;
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
